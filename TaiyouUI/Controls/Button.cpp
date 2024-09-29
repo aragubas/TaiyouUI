dd6cf1be97bf52fbb15e69fb7b011b400dbbfe9d
Aragubas <47148297+aragubas@users.noreply.github.com>
2024-09-29 01:36:00 -0300

    WIP Add font rendering code

--- TaiyouUI/Controls/Button.cpp
+++ TaiyouUI/Controls/Button.cpp
@@ -1,12 +1,26 @@
 #include "Button.h"
 #include <stdio.h>
 
-Button::Button()
+Button::Button(struct UIRootContext context) : m_Text(std::string()), m_TextTexture(nullptr)
 {
-    backgroundRect = SDL_Rect();
     MinimumSize = SDL_FPoint();
     MinimumSize.x = 500;
     MinimumSize.y = 120;
+
+    m_Font = TTF_OpenFont("./Application Data/Fonts/Inter-Variable.ttf", 20);
+    if (m_Font == NULL)
+    {
+        throw "Default font not found";
+    }
+}
+
+Button::~Button()
+{
+    SDL_DestroyTexture(m_TextTexture);
+}
+
+void Button::EventUpdate(SDL_Event &event)
+{
 }
 
 void Button::Update(double deltaTime)
@@ -22,4 +36,35 @@ void Button::OnDraw(SDL_Renderer *renderer, double deltaTime)
     size.w = Size.x;
     size.h = Size.y;
     SDL_RenderFillRect(renderer, &size);
+}
+
+void Button::SetText(std::string text)
+{
+    m_Text = text;
+
+    // Free old text texture
+    SDL_DestroyTexture(m_TextTexture);
+
+    SDL_Color foregroundColor = SDL_Color();
+    foregroundColor.a = 255;
+    foregroundColor.r = 255;
+    foregroundColor.g = 255;
+    foregroundColor.b = 255;
+
+    SDL_Color backgroundColor = SDL_Color();
+    backgroundColor.a = 0;
+    backgroundColor.r = 0;
+    backgroundColor.g = 0;
+    backgroundColor.b = 0;
+
+    SDL_Surface *fontSurface = TTF_RenderText_LCD(m_Font, m_Text.c_str(), foregroundColor, backgroundColor);
+
+    // SDL_CreateTextureFromSurface()
+
+    SDL_FreeSurface(fontSurface);
+}
+
+std::string Button::GetText() const
+{
+    return m_Text;
 }
--- TaiyouUI/Controls/Button.h
+++ TaiyouUI/Controls/Button.h
@@ -1,18 +1,26 @@
 #pragma once
 #include <string>
 #include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL.h>
+#include <iostream>
 #include "../Control.h"
+#include "../UIRootContext.h"
 
 class Button : public Control
 {
-    SDL_Rect backgroundRect;
+    SDL_Texture *m_TextTexture;
+    TTF_Font *m_Font;
+    std::string m_Text;
 
 public:
-    std::string Text;
-
-    Button();
+    Button(struct UIRootContext context);
+    ~Button();
 
     void Update(double deltaTime) override;
+    void EventUpdate(SDL_Event &event) override;
+
+    void SetText(std::string Text);
+    std::string GetText() const;
 
 private:
     void OnDraw(SDL_Renderer *renderer, double deltaTime) override;
